331e9c2725e2b5dc9790c0b98a5949be15689374
Rachel Peppe <rapeppe@hotmail.com>
2024-04-01 11:02:56 -0300

    commit inicial

---
+++ typescript/src/exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplicaNumeros(num1, num2) {
+    num1 = 2;
+    num2 = 5;
+    return num1 * num2;
+}
+console.log(multiplicaNumeros);
---
+++ typescript/src/exercicio.ts
@@ -0,0 +1,13 @@
+function multiplicaNumeros(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+const resultado = multiplicaNumeros(10, 12);
+console.log(resultado);
+
+function saudacao(nome: string): string {
+    return "Olá" + nome;
+}
+
+const mensagem = saudacao("Rachel");
+console.log(mensagem);
